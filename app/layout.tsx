185bf2829aa96545c552cd754b809fa25162f4b6
Ondřej Krejčí <ondrakrejci83@seznam.cz>
2025-04-29 08:34:22 +0200

    google analythics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -49,6 +50,15 @@ export default function RootLayout({
   return (
     <html lang="cs">
       <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K3TVZQ2T');
+          `}
+        </Script>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/logo/coalshift_logo_favicon-color.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/logo/coalshift_logo_favicon-color.svg" type="image/svg+xml" />
@@ -59,6 +69,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K3TVZQ2T"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
