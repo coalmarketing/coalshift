b89cae63a63fad8518a8d147c6f78d98a642bcd3
Ondřej Krejčí <ondrej.krejci@coalsoft.cz>
2025-06-12 12:50:54 +0200

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K3TVZQ2T');
+            })(window,document,'script','dataLayer','GTM-NQDZKVLF');
           `}
         </Script>
         <Script src="//webform.onquanda.com/static/js/webform/embedded.min.js" strategy="beforeInteractive" />
@@ -74,7 +74,7 @@ export default function RootLayout({
       >
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K3TVZQ2T"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NQDZKVLF"
             height="0" 
             width="0" 
             style={{ display: 'none', visibility: 'hidden' }}
